1f45b6b2da59261c1dccc28a91d14c2cadba7a12
SamLab17 <samlaberge@live.com>
2019-04-01 17:01:11 -0500

    oops i guess i was missing a div tab before

--- src/App.js
+++ src/App.js
@@ -72,6 +72,20 @@ class App extends React.Component {
         }]
       }
     };
+    const horizData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'My First dataset',
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: [65, 59, 80, 81, 56, 55, 40]
+        }
+      ]
+    };
 
     return this.state.currentPage === "home" ? (
         <div>
@@ -80,7 +94,7 @@ class App extends React.Component {
         <BarChart title="My Bar Graph" height="300" data = {barData} options={options}/>
         </div>
       ): (
-        <Sidebar updatePage={this.updatePage} currentPage={this.state.currentPage}/>
+          <Sidebar updatePage={this.updatePage} currentPage={this.state.currentPage}/>
     );
   }
 }
