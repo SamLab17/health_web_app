6531f7a5e63b94c12b54827396539745c66a7644
SamLab17 <samlaberge@live.com>
2019-04-08 19:15:23 -0500

    Improved form for selecting dietary restrictions. Added a fetch request for getting user info using axios.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,120 +1,168 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import DoughnutChart from "./DoughnutChart";
 import BarChart from "./BarChart";
 import FoodCard from "./FoodCard";
-import {Grid, Card,Statistic} from "semantic-ui-react";
+import { Grid, Card, Statistic } from "semantic-ui-react";
 
-class Home extends Component{
-    render(){
-        let data = [  //Data just for testing the doughnut graph
-            {
-                value: 300,
-                color:"#f8971f",
-                highlight: "#f8971f",
-                label: "Orange"
-            },
-            {
-                value: 50,
-                color: "#ffd600",
-                highlight: "#ffd600",
-                label: "Yellow"
-            },
-            {
-                value: 100,
-                color: "#a6cd57",
-                highlight: "#a6cd57",
-                label: "Green"
+class Home extends Component {
+  render() {
+    let data = [
+      //Data just for testing the doughnut graph
+      {
+        value: 200,
+        color: "#f8971f",
+        highlight: "#f8971f",
+        label: "Orange"
+      },
+      {
+        value: 50,
+        color: "#ffd600",
+        highlight: "#ffd600",
+        label: "Yellow"
+      },
+      {
+        value: 100,
+        color: "#a6cd57",
+        highlight: "#a6cd57",
+        label: "Green"
+      },
+      {
+        value: 80,
+        color: "rgb(0, 169, 183)",
+        highlight: "rgb(0, 169, 183)",
+        label: "Blue"
+      }
+    ];
+
+    let barData = {
+      labels: [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+      ],
+      datasets: [
+        {
+          label: "# of Votes",
+          fillColor: [
+            "rgba(248, 151, 31, 0.5)",
+            "rgba(255, 214, 0,0.5)",
+            "rgba(166, 205, 87,0.5)",
+            "rgba(87, 157, 66,0.5)",
+            "rgba(0, 169, 183,0.5)",
+            "rgba(0, 95, 134,0.5)",
+            "rgba(156, 173, 183,0.5)"
+          ],
+          strokeColor: [
+            "rgb(248, 151, 31)",
+            "rgb(255, 214, 0)",
+            "rgb(166, 205, 87)",
+            "rgb(87, 157, 66)",
+            "rgb(0, 169, 183)",
+            "rgb(0, 95, 134)",
+            "rgb(156, 173, 183)"
+          ],
+          borderWidth: 1,
+          data: [12, 20, 3, 5, 2, 3, 6]
+        }
+      ]
+    };
+    let options = {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
             }
-        ];
+          }
+        ]
+      }
+    };
 
-        let barData = {
-            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-            datasets: [{
-                label: '# of Votes',
-                fillColor:[
-                    "rgba(248, 151, 31, 0.5)",
-                    "rgba(255, 214, 0,0.5)",
-                    "rgba(166, 205, 87,0.5)",
-                    "rgba(87, 157, 66,0.5)",
-                    "rgba(0, 169, 183,0.5)",
-                    "rgba(0, 95, 134,0.5)",
-                    "rgba(156, 173, 183,0.5)",
-                ],
-                strokeColor: [
-                    "rgb(248, 151, 31)",
-                    "rgb(255, 214, 0)",
-                    "rgb(166, 205, 87)",
-                    "rgb(87, 157, 66)",
-                    "rgb(0, 169, 183)",
-                    "rgb(0, 95, 134)",
-                    "rgb(156, 173, 183)",
-                ],
-                borderWidth: 1,
-                data: [12, 20, 3, 5, 2, 3, 6]
-            }]
-        };
-        let options= {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        };
+    return (
+      <div>
+        <h1> Home </h1>
+        <Grid columns={2} padded>
+          <Grid.Column verticalAlign={"middle"} floated={"left"}>
+            <Grid.Row>
+              <Grid columns={2} padded={"vertically"}>
+                <Grid.Column>
+                  <DoughnutChart
+                    title="My Graph"
+                    data={data}
+                    options={{
+                      animationEasing: "easeOutExpo",
+                      percentageInnerCutout: 70
+                    }}
+                  />
+                </Grid.Column>
+                <Grid.Column verticalAlign={"middle"} textAlign={"center"}>
+                  <Statistic>
+                    <Statistic.Value>5,500</Statistic.Value>
+                    <Statistic.Label>Points</Statistic.Label>
+                  </Statistic>
+                </Grid.Column>
+              </Grid>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid columns={2} padded={"vertically"}>
+                <Grid.Column>
+                  <BarChart
+                    title="My Bar Graph"
+                    height="300"
+                    data={barData}
+                    options={options}
+                  />
+                </Grid.Column>
+                <Grid.Column verticalAlign={"middle"} textAlign={"center"}>
+                  <Statistic>
+                    <Statistic.Value>20</Statistic.Value>
+                    <Statistic.Label>Day Pulse</Statistic.Label>
+                  </Statistic>
+                </Grid.Column>
+              </Grid>
+            </Grid.Row>
+          </Grid.Column>
 
-        return (
-            <div>
-                <h1> Home </h1>
-                <Grid columns={2} padded >
-                    <Grid.Column verticalAlign={"middle"} floated={"left"}>
-                        <Grid.Row>
-                            <Grid columns={2} padded={"vertically"}>
-                                <Grid.Column>
-                                    <DoughnutChart  title="My Graph" data = {data} options={{animationEasing: "easeOutExpo", percentageInnerCutout: 70}}/>
-                                </Grid.Column>
-                                <Grid.Column verticalAlign={"middle"} textAlign={"center"}>
-                                    <Statistic>
-                                        <Statistic.Value>5,500</Statistic.Value>
-                                        <Statistic.Label>Points</Statistic.Label>
-                                    </Statistic>
-                                </Grid.Column>
-                            </Grid>
-                        </Grid.Row>
-                        <Grid.Row>
-                            <Grid columns={2} padded={"vertically"}>
-                                <Grid.Column>
-                                    <BarChart title="My Bar Graph" height="300" data = {barData} options={options}/>
-                                </Grid.Column>
-                                <Grid.Column verticalAlign={"middle"} textAlign={"center"}>
-                                    <Statistic>
-                                        <Statistic.Value>20</Statistic.Value>
-                                        <Statistic.Label>Day Pulse</Statistic.Label>
-                                    </Statistic>
-                                </Grid.Column>
-                            </Grid>
-
-                        </Grid.Row>
-                    </Grid.Column>
-
-                    <Grid.Column verticalAlign={"top"} floated={"right"}  textAlign={"left"}>
-                        <Card.Group  itemsPerRow={"2"}>
-                        <FoodCard ImageSource={"carrot.jpg"} Title="Carrot" Subtitle="Side Dish"
-                                  Description="Vegan, Vegetarian"/>
-                            <FoodCard ImageSource={"carrot.jpg"} Title="Carrot" Subtitle="Vegan, Vegetarian"
-                                      Description="Side Dish"/>
-                            <FoodCard ImageSource={"carrot.jpg"} Title="Carrot" Subtitle="Side Dish"
-                                      Description="I am a carrot and I am orange but I am not an orange." />
-                            <FoodCard ImageSource={"potato.jpg"} Title="Potato" Subtitle="Side Dish"
-                                  Description="I'm a real spud. But I can also be a real chip on your shoulder." />
-                        </Card.Group>
-                    </Grid.Column>
-                </Grid>
-            </div>
-
-        );
-
-    }
+          <Grid.Column
+            verticalAlign={"top"}
+            floated={"right"}
+            textAlign={"left"}
+          >
+            <Card.Group itemsPerRow={"2"}>
+              <FoodCard
+                ImageSource={"carrot.jpg"}
+                Title="Carrot"
+                Subtitle="Side Dish"
+                Description="Vegan, Vegetarian"
+              />
+              <FoodCard
+                ImageSource={"carrot.jpg"}
+                Title="Carrot"
+                Subtitle="Vegan, Vegetarian"
+                Description="Side Dish"
+              />
+              <FoodCard
+                ImageSource={"carrot.jpg"}
+                Title="Carrot"
+                Subtitle="Side Dish"
+                Description="I am a carrot and I am orange but I am not an orange."
+              />
+              <FoodCard
+                ImageSource={"potato.jpg"}
+                Title="Potato"
+                Subtitle="Side Dish"
+                Description="I'm a real spud. But I can also be a real chip on your shoulder."
+              />
+            </Card.Group>
+          </Grid.Column>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Home;
--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -1,48 +1,107 @@
-import React from "react";
-import {Checkbox, Grid, Form, Input} from 'semantic-ui-react';
+import React, {Component} from "react";
+import {Checkbox, Grid, Form, Input, Button} from 'semantic-ui-react';
+import axios from 'axios';
 
-const Preferences = () => {
-    return (
-       <div>
-        <h1>Preferences</h1>
+class Preferences extends Component {
 
-        <Grid columns={2} divided>
-            <Grid.Column>
-            <h3>Personal Information</h3>
-            <Form>
-                <Form.Group unstackable width={'equal'}>
-                    <Form.Field required>
-                        <label>First name</label>
-                        <Input placeholder={'First name'}/>
-                    </Form.Field>
-                    <Form.Field >
-                        <label>Last name</label>
-                        <Input placeholder={'Last name'}/>
-                    </Form.Field>
-                </Form.Group>
-                <Form.Field >
-                    <label>UT EID</label>
-                    <Input readOnly placeholder={'EID'}/>
-                </Form.Field>
-                <Form.Field >
-                    <label>E-mail</label>
-                    <Input placeholder={'email'}/>
-                </Form.Field>
-            </Form>
-            </Grid.Column>
-            <Grid.Column>
-                <h3>Dietary Information</h3>
-                <Form>
-                    <Form.Field control={Checkbox} label={"Vegan"}/>
-                    <Form.Field control={Checkbox} label={"Vegetarian"}/>
-                    <Form.Field control={Checkbox} label={"Lactose Intolerant"}/>
-                    <Form.Field control={Checkbox} label={"Glucose Intolerant"}/>
+    state = {
+        personal_info: {
+            firstName: 'First Name',
+            lastName: 'Last Name',
+            id: '0',
+            email: 'test@test.com',
+        },
+        dietary_info: {
+            beef: false,
+            pork: false,
+            fish: false,
+            eggs: false,
+            milk: false,
+            soy: false,
+            gluten: false,
+            nuts: false
+        }
+    };
 
-                </Form>
-            </Grid.Column>
-        </Grid>
-       </div>
-    )
+    componentDidMount(){
+        axios.get("https://jsonplaceholder.typicode.com/users")
+            .then(response => {
+                console.log(response);
+                let first = response.data[0];
+                this.setState({firstName: first.name.split(" ")[0], lastName: first.name.split(" ")[1], id: first.id, email: first.email});
+            })
+    }
+
+    useDietaryPreset = (e, data) => {
+            if (data.name === 'vegan') {
+                this.setState({dietary_info: {beef: true, pork: true, fish: true, eggs: true,
+                        milk: true, soy: false, gluten: false, nuts: false}});
+            } else if (data.name === 'vegetarian') {
+                this.setState({dietary_info: {beef: true, pork: true, fish: true, eggs: false,
+                        milk: false, soy: false, gluten: false, nuts: false}});
+            }
+    };
+
+    clearAll = () => {
+        this.setState({dietary_info:{beef: false, pork: false, eggs: false, fish: false, milk:
+                    false, soy: false, nuts: false, gluten: false, vegan: false, vegetarian: false}});
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Preferences</h1>
+
+                <Grid columns={2} divided>
+                    <Grid.Column>
+                        <h3>Personal Information</h3>
+                        <Form>
+                            <Form.Group unstackable width={'equal'}>
+                                <Form.Field required>
+                                    <label>First name</label>
+                                    <Input placeholder={this.state.firstName}/>
+                                </Form.Field>
+                                <Form.Field>
+                                    <label>Last name</label>
+                                    <Input placeholder={this.state.lastName}/>
+                                </Form.Field>
+                            </Form.Group>
+                            <Form.Field>
+                                <label>UT EID</label>
+                                <Input readOnly placeholder={this.state.id}/>
+                            </Form.Field>
+                            <Form.Field>
+                                <label>E-mail</label>
+                                <Input placeholder={this.state.email}/>
+                            </Form.Field>
+                            <Button color={'orange'} type={"submit"} disabled> Apply Changes</Button>
+                        </Form>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <h3>Dietary Information</h3>
+                        <h4>Dietary Presets</h4>
+                        <Form>
+                            <Button basic color={'orange'} onClick={this.useDietaryPreset} name={"vegan"}>Vegan</Button>
+                            <Button basic color={'orange'} onClick={this.useDietaryPreset} name={"vegetarian"}>Vegetarian</Button>
+                        </Form>
+                        <h4>The following checked items will never appear in your recommendations. </h4>
+                        <Form>
+                            <Form.Field control={Checkbox} label={"Beef"} checked={this.state.dietary_info.beef}/>
+                            <Form.Field control={Checkbox} label={"Pork"} checked={this.state.dietary_info.pork}/>
+                            <Form.Field control={Checkbox} label={"Fish/Shellfish"} checked={this.state.dietary_info.fish}/>
+                            <Form.Field control={Checkbox} label={"Eggs"} checked={this.state.dietary_info.eggs}/>
+                            <Form.Field control={Checkbox} label={"Milk"} checked={this.state.dietary_info.milk}/>
+                            <Form.Field control={Checkbox} label={"Soy"} checked={this.state.dietary_info.soy}/>
+                            <Form.Field control={Checkbox} label={"Gluten"} checked={this.state.dietary_info.gluten}/>
+                            <Form.Field control={Checkbox} label={"Peanuts/Tree Nuts"} checked={this.state.dietary_info.nuts}/>
+                            <Button color={'orange'} type={"submit"}>Apply Changes</Button>
+                            <Button onClick={this.clearAll}>Clear All</Button>
+                        </Form>
+                    </Grid.Column>
+                </Grid>
+            </div>
+        )
+    }
 };
 
 export default Preferences;
