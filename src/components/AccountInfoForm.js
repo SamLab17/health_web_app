1c2718769a35f6e344670df1c53b91784765a7f7
SamLab17 <samlaberge@live.com>
2019-04-10 15:57:15 -0500

    Form validation (Has bugs)

--- src/components/AccountInfoForm.js
+++ src/components/AccountInfoForm.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
 import { Form, Input, Button } from "semantic-ui-react";
 import SubmissionMessage from "./SubmissionMessage";
+import ValidatedInputField from "./ValidatedInput";
 
 class AccountInfoForm extends Component {
   state = {
     personal_info: {
       //This user's personal information
-      firstName: "First Name",
-      lastName: "Last Name",
+      firstName: "Jane",
+      lastName: "Doe",
       id: "0",
       email: "default@default.com"
     },
+    fieldIsValid: {
+      firstName: true,
+      lastName: true,
+      id: true,
+      email: true
+    },
+    fieldsChanged: false,
+    allFieldsValid: true,
     trySubmit: false, //We are trying/tried to send new personal information to the rest API
     didSubmit: false //If true, updating personal info was successful
   };
@@ -18,6 +27,7 @@ class AccountInfoForm extends Component {
   componentDidMount() {
     //Retrieve account information
   }
+
   submitInfo = () => {
     this.setState({ trySubmit: true });
     //Try Submitting to rest API
@@ -25,30 +35,66 @@ class AccountInfoForm extends Component {
     //If Successful
     this.setState({ didSubmit: true });
   };
+
+  //This is broken, setting state does not work
+  handleChange = (field, input, isValid) => {
+    this.setState({ fieldIsValid: { field: isValid }, fieldsChanged: true });
+    console.log(this.state.fieldIsValid);
+    if (isValid) this.setState({ personal_info: { [field]: input } });
+    console.log(field, input, isValid);
+    this.checkAllFieldsValid();
+    console.log(this.state.fieldsChanged, this.state.allFieldsValid);
+  };
+
+  checkAllFieldsValid() {
+    const { firstName, lastName, id, email } = this.state.fieldIsValid;
+    console.log(firstName, lastName, id, email);
+    this.setState({ allFieldsValid: firstName && lastName && id && email });
+  }
+
   render() {
     return (
       <div>
         <h3>Account Information</h3>
         <Form>
-          <Form.Group unstackable width={"equal"}>
-            <Form.Field required>
-              <label>First name</label>
-              <Input placeholder={this.state.firstName} />
-            </Form.Field>
-            <Form.Field>
-              <label>Last name</label>
-              <Input placeholder={this.state.lastName} />
-            </Form.Field>
+          <Form.Group unstackable width={"equal"} placeholder="Test text">
+            <ValidatedInputField
+              label="First name"
+              field="firstName"
+              required
+              default={this.state.personal_info.firstName}
+              onChange={this.handleChange}
+            />
+            <ValidatedInputField
+              label="Last name"
+              field="lastName"
+              required
+              default={this.state.personal_info.lastName}
+              onChange={this.handleChange}
+            />
           </Form.Group>
-          <Form.Field>
-            <label>UT EID</label>
-            <Input readOnly placeholder={this.state.id} />
-          </Form.Field>
-          <Form.Field>
-            <label>E-mail</label>
-            <Input placeholder={this.state.email} />
-          </Form.Field>
-          <Button color={"orange"} type={"submit"} onClick={this.submitInfo}>
+          <ValidatedInputField
+            label="UT EID"
+            field="id"
+            required
+            readOnly
+            default={this.state.personal_info.id}
+            onChange={this.handleChange}
+          />
+          <ValidatedInputField
+            label="E-mail"
+            field="email"
+            required
+            email
+            default={this.state.personal_info.email}
+            onChange={this.handleChange}
+          />
+          <Button
+            disabled={!this.state.allFieldsValid || !this.state.fieldsChanged}
+            color={"orange"}
+            type={"submit"}
+            onClick={this.submitInfo}
+          >
             Update Account Information
           </Button>
         </Form>
---
+++ src/components/ValidatedInput.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Input, Form } from "semantic-ui-react";
+
+/*
+Props:
+required: boolean
+email: boolean
+number: boolean
+readOnly: boolean
+label: string
+placeholder: string
+onChange: function
+
+*/
+
+class ValidatedInput extends Component {
+  state = {
+    isValid: true,
+    changed: false,
+    currentInput: "" //May not be necessary
+  };
+
+  handleChange = (event, data) => {
+    this.setState({ changed: true, currentInput: data.value });
+    this.validate(data.value);
+    this.props.onChange(this.props.field, data.value, this.state.isValid);
+  };
+
+  validate(input) {
+    let isValid = true;
+    if (this.props.required && input === "") isValid = false;
+    if (isValid && this.props.email) isValid = this.checkEmail(input);
+    if (this.props.number && !isNaN(input)) isValid = true;
+    this.setState({ isValid: isValid });
+  }
+
+  checkEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+
+  render() {
+    return (
+      <Form.Field
+        disabled={this.props.readOnly}
+        required={this.props.required}
+        error={!this.state.isValid && this.state.changed}
+      >
+        <label>{this.props.label}</label>
+        <Input onChange={this.handleChange} defaultValue={this.props.default} />
+      </Form.Field>
+    );
+  }
+}
+
+export default ValidatedInput;
